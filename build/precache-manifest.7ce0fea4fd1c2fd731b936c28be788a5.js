4adee5aab41f2dc99628b67cfac26f705aee1634
bhkoh <boonhui.koh.2013@sis.smu.edu.sg>
2019-11-24 01:35:04 +0800

    second commit

---
+++ build/precache-manifest.7ce0fea4fd1c2fd731b936c28be788a5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e4f41e486c25faed0aea5f05b0fed7",
+    "url": "/fdtracker/index.html"
+  },
+  {
+    "revision": "4fb86078989464d0fd55",
+    "url": "/fdtracker/static/css/2.8024a5e8.chunk.css"
+  },
+  {
+    "revision": "1e2e34dec2df3bafb69a",
+    "url": "/fdtracker/static/css/main.5989cb28.chunk.css"
+  },
+  {
+    "revision": "4fb86078989464d0fd55",
+    "url": "/fdtracker/static/js/2.aac3d3dd.chunk.js"
+  },
+  {
+    "revision": "1e2e34dec2df3bafb69a",
+    "url": "/fdtracker/static/js/main.8c93a75c.chunk.js"
+  },
+  {
+    "revision": "fae96fdfb742055abfae",
+    "url": "/fdtracker/static/js/runtime~main.3d324bb6.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/fdtracker/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/fdtracker/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/fdtracker/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/fdtracker/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/fdtracker/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "0f16597876e9bb7da30d38ce7de5e954",
+    "url": "/fdtracker/static/media/fdlogo.0f165978.jpeg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/fdtracker/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/fdtracker/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/fdtracker/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/fdtracker/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/fdtracker/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/fdtracker/static/media/sidebar-1.6be21e8a.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/fdtracker/static/media/sidebar-2.7a4ce7cc.jpg"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/fdtracker/static/media/sidebar-3.34535b95.jpg"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/fdtracker/static/media/sidebar-4.7bc259fd.jpg"
+  }
+]);
